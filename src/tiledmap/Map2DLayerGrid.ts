c0704330e51ebf76aef39ef06cf1f65e21f67aa5
Wolfger Schramm <wolfger@spearwolf.de>
2018-10-25 19:27:44 +0200

    refactor Map2DLayerGrid#update()

--- src/tiledmap/Map2DLayerGrid.ts
+++ src/tiledmap/Map2DLayerGrid.ts
@@ -1,5 +1,3 @@
-import * as THREE from 'three';
-
 import { IMap2DLayer } from './IMap2DLayer';
 import { Map2DGridTile } from './Map2DGridTile';
 import { Map2DView } from './Map2DView';
@@ -42,41 +40,34 @@ export class Map2DLayerGrid {
   update() {
     // I. create visible map tiles (and remove/dispose unvisible)
     //
-    const gridTileSize = new THREE.Vector2(this.gridTileWidth, this.gridTileHeight);
-    const halfDimension = this.view.dimension.clone();
-    halfDimension.multiplyScalar(0.5);
+    const viewHalfWidth = this.view.dimension.x * 0.5;
+    const viewHalfHeight = this.view.dimension.y * 0.5;
 
-    const topLeft = this.view.origin.clone();
-    topLeft.sub(halfDimension);
-    topLeft.divide(gridTileSize);
-    topLeft.set(Math.floor(topLeft.x), Math.floor(topLeft.y));
+    const left = Math.floor((this.view.origin.x - viewHalfWidth) / this.gridTileWidth);
+    const top = Math.floor((this.view.origin.y - viewHalfHeight) / this.gridTileHeight);
+    const right = Math.floor((this.view.origin.x + viewHalfWidth) / this.gridTileWidth);
+    const bottom = Math.floor((this.view.origin.y + viewHalfHeight) / this.gridTileHeight);
 
-    const bottomRight = this.view.origin.clone();
-    bottomRight.add(halfDimension);
-    bottomRight.divide(gridTileSize);
-    bottomRight.set(Math.floor(bottomRight.x), Math.floor(bottomRight.y));
+    const width = right - left + 1;
+    const height = bottom - top + 1;
 
-    const sizes = bottomRight.clone();
-    sizes.sub(topLeft);
-    sizes.addScalar(1);
-
-    console.log('[gridTiles] topLeft', topLeft.x, topLeft.y, 'bottomRight', bottomRight.x, bottomRight.y, 'sizes', sizes.x, sizes.y);
+    console.log('[gridTiles] topLeft', left, top, 'bottomRight', right, bottom, 'sizes', width, height);
     // tslint:disable-next-line:max-line-length
-    console.log('[tiles] topLeft', topLeft.x * this.gridTileColumns, topLeft.y * this.gridTileRows, 'bottomRight', bottomRight.x * this.gridTileColumns, bottomRight.y * this.gridTileRows, 'sizes', sizes.x * this.gridTileColumns, sizes.y * this.gridTileRows);
+    console.log('[tiles] topLeft', left * this.gridTileColumns, top * this.gridTileRows, 'bottomRight', right * this.gridTileColumns, bottom * this.gridTileRows, 'sizes', width * this.gridTileColumns, height * this.gridTileRows);
     // tslint:disable-next-line:max-line-length
-    console.log('[pixels] topLeft', topLeft.x * this.gridTileWidth, topLeft.y * this.gridTileHeight, 'bottomRight', bottomRight.x * this.gridTileWidth, bottomRight.y * this.gridTileHeight, 'sizes', sizes.x * this.gridTileWidth, sizes.y * this.gridTileHeight);
+    console.log('[pixels] topLeft', left * this.gridTileWidth, top * this.gridTileHeight, 'bottomRight', right * this.gridTileWidth, bottom * this.gridTileHeight, 'sizes', width * this.gridTileWidth, height * this.gridTileHeight);
 
     // II. create geometries for all *new* map tiles
     //
-    const prevGridTiles = this.gridTiles.slice(0);
+    const prevTiles = this.gridTiles.slice(0);
     const newTiles: Map2DGridTile[] = [];
     const knownTiles: Map2DGridTile[] = [];
 
-    for (let yOffset = 0; yOffset < sizes.y; ++yOffset) {
-      for (let xOffset = 0; xOffset < sizes.x; ++xOffset) {
-        const x = topLeft.x + xOffset;
-        const y = topLeft.y + yOffset;
-        let tile = takeFrom(prevGridTiles, x, y);
+    for (let yOffset = 0; yOffset < height; ++yOffset) {
+      for (let xOffset = 0; xOffset < width; ++xOffset) {
+        const x = left + xOffset;
+        const y = top + yOffset;
+        let tile = takeFrom(prevTiles, x, y);
         if (!tile) {
           // maybe re-use obsolete tiles?
           tile = new Map2DGridTile(this.layer, this.gridTileColumns, this.gridTileRows);
