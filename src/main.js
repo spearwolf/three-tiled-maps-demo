36d831e9c0b6b8c7cbe0b919f2d5b3b436f84a30
Wolfger Schramm <wolfger@spearwolf.de>
2019-05-14 18:33:21 +0200

    lab-wall-tiles-new and invert camera

--- src/main.js
+++ src/main.js
@@ -22,8 +22,8 @@ console.log('hej ho 🦄');
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x203040);
 
-const camera3d = new THREE.PerspectiveCamera(75, 1, 0.1, 9999);
-camera3d.position.set(0, -75, 350);
+const camera3d = new THREE.PerspectiveCamera(75, 1, 1, 1000);
+camera3d.position.set(0, 75, 350);
 camera3d.lookAt(0, 0, 0);
 camera3d.up.set(0, 0, 1);
 
@@ -32,6 +32,9 @@ const min = (a, b) => a > b ? b : a;
 const halfSize = min(VIEW_WIDTH, calcViewHeight()) / 2;
 const cam2dZ = 100;
 const camera2d = new THREE.OrthographicCamera(-halfSize, halfSize, halfSize, -halfSize, 1, 1000 );
+const m = new THREE.Matrix4();
+m.makeScale(-1, -1, 1);
+camera2d.applyMatrix(m);
 
 let curCamera = camera3d;
 
@@ -41,8 +44,8 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 // see https://threejs.org/docs/#examples/loaders/GLTFLoader
-renderer.gammaOutput = true;
-renderer.gammaFactor = 2.2;
+// renderer.gammaOutput = true;
+// renderer.gammaFactor = 2.2;
 
 const DPR = window.devicePixelRatio || 1;
 renderer.setPixelRatio(1);
@@ -138,10 +141,10 @@ function render(time) {
     controls.update();
 
     if (view) {
-      view.centerY += speedNorth * t;
-      view.centerY -= speedSouth * t;
-      view.centerX += speedEast * t;
-      view.centerX -= speedWest * t;
+      view.centerY -= speedNorth * t;
+      view.centerY += speedSouth * t;
+      view.centerX -= speedEast * t;
+      view.centerX += speedWest * t;
       view.update();
 
       camera2d.position.set(view.centerX, view.centerY, cam2dZ);
@@ -163,35 +166,58 @@ requestAnimationFrame(animate);
 // load tiled map ////////////////////////////////////////////////////
 
 Promise.all([
-  TiledMap.load('./maps/180917-a-first-map.json'),
-  TextureLibrary.load('sketch-tiles.json', './atlas/'),
+  // TiledMap.load('./maps/180917-a-first-map.json'),
+  // TextureLibrary.load('sketch-tiles.json', './atlas/'),
+  TiledMap.load('./maps/lab-wall-tiles-new-map.json'),
+  TextureLibrary.load('lab-wall-tiles-new.json', './atlas/'),
 ]).then(([tiledMap, texLib]) => {
 
   const { imgEl } = texLib.atlas.baseTexture;
   const el = document.querySelector('.textureAtlas');
   el.appendChild(imgEl);
 
+  // texLib.setIdNameMap([
+  //   [1, 'empty.png'],
+  //   [2, 'floor.png'],
+  //   [3, 'wall-e.png'],
+  //   [4, 'wall-n.png'],
+  //   [5, 'wall-ns.png'],
+  //   [6, 'wall-s.png'],
+  //   [7, 'wall-w.png'],
+  //   [8, 'wall-es.png'],
+  //   [9, 'wall-esw.png'],
+  //   [10, 'wall-ew.png'],
+  //   [11, 'wall-ne.png'],
+  //   [12, 'wall-nes.png'],
+  //   [13, 'wall-nesw.png'],
+  //   [14, 'wall-new.png'],
+  //   [15, 'wall-nsw.png'],
+  //   [16, 'wall-nw.png'],
+  //   [17, 'wall-sw.png'],
+  // ]);
   texLib.setIdNameMap([
-    [1, 'empty.png'],
-    [2, 'floor.png'],
-    [3, 'wall-e.png'],
-    [4, 'wall-n.png'],
-    [5, 'wall-ns.png'],
-    [6, 'wall-s.png'],
-    [7, 'wall-w.png'],
-    [8, 'wall-es.png'],
-    [9, 'wall-esw.png'],
-    [10, 'wall-ew.png'],
-    [11, 'wall-ne.png'],
-    [12, 'wall-nes.png'],
-    [13, 'wall-nesw.png'],
-    [14, 'wall-new.png'],
-    [15, 'wall-nsw.png'],
-    [16, 'wall-nw.png'],
-    [17, 'wall-sw.png'],
+    [1, 'lab-wall-tiles-00.png'],
+    [2, 'lab-wall-tiles-01.png'],
+    [3, 'lab-wall-tiles-02.png'],
+    [4, 'lab-wall-tiles-03.png'],
+    [5, 'lab-wall-tiles-04.png'],
+    [6, 'lab-wall-tiles-05.png'],
+    [7, 'lab-wall-tiles-06.png'],
+    [8, 'lab-wall-tiles-07.png'],
+    [9, 'lab-wall-tiles-08.png'],
+    [10, 'lab-wall-tiles-09.png'],
+    [11, 'lab-wall-tiles-10.png'],
+    [12, 'lab-wall-tiles-11.png'],
+    [13, 'lab-wall-tiles-12.png'],
+    [14, 'lab-wall-tiles-13.png'],
+    [15, 'lab-wall-tiles-14.png'],
+    [16, 'lab-wall-tiles-15.png'],
+    [17, 'lab-wall-tiles-16.png'],
+    [18, 'lab-wall-tiles-18.png'],
   ]);
 
-  texLib.setDefaultTexture('empty.png');
+  // texLib.setDefaultTexture('empty.png');
+  texLib.setDefaultTexture('sketch-dungeon-back-tile.png');
 
   console.log('TextureLibrary', texLib);
 
@@ -202,10 +228,13 @@ Promise.all([
   map2d.appendLayer(layerMain);
 
   view = new Map2DView(map2d, 0, 0, VIEW_WIDTH, calcViewHeight(), 100, 100);
-  view.addLayer(new Map2DViewLayer(view, layerMain, tiledMap.getLayer('main')));
+
+  // view.addLayer(new Map2DViewLayer(view, layerMain, tiledMap.getLayer('main')));
+  // view.addLayer(new Map2DViewLayer(view, layerMain, tiledMap.getLayer('Kachelebene 2')));
+  view.addLayer(new Map2DViewLayer(view, layerMain, tiledMap.getLayer('Kachelebene 1')));
   // view.update();
 
-  viewFrame = new Map2DViewFrame(map2d);
+  viewFrame = new Map2DViewFrame(map2d, 0xff0000, 2);
   map2d.add(viewFrame);
 
   rendererShouldRender = true;
